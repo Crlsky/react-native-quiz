05caa7993b397041fe1f7dd6a722c713aecaeb18
Karol <karol.swieciochowski@gmail.com>
2021-05-11 08:05:59 +0200

    finished

--- App.js
+++ App.js
@@ -2,54 +2,75 @@ import * as React from 'react';
 import { View, Text, Button } from 'react-native';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
-const questionsLib = [
-  {id: 1, question: "Czy pies szczeka?", answers: 1},
-  {id: 2, question: "Czy kot szczeka?", correct: 0},
-  {id: 3, question: "Czy kot miałczy?", correct: 1},
-  {id: 4, question: "czy pies miałczy?", correct: 0},
-]
+import react from 'react';
 
 let score = 0;
 
-function QuizzScreen({route, navigation}) {
-  const { questionNumber, answer } = route.params;
-  console.log(questionNumber,answer);
+class QuizzScreen extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      'navigation': props.navigation,
+      'questionNumber': props.route.params.questionNumber, 
+      'questionsLib':[
+        {id: 1, question: "Is dog barking?", correct: 'yes'},
+        {id: 2, question: "Is cat barking?", correct: 'no'},
+        {id: 3, question: "Is cat meows?", correct: 'yes'},
+        {id: 4, question: "Is dog meows?", correct: 'no'},
+      ]
+    };
+    this.QuizzValidator = this.QuizzValidator.bind(this);
+  }
 
-  if(questionNumber<=4 && answer && questionsLib[questionNumber-1].correct == answer)
-    score+=1;
+  QuizzValidator(event){
+    if(this.state.questionsLib[this.state.questionNumber-1].correct == event)
+      score +=1;
+  
+    if(this.state.questionNumber<=4)
+      this.state.navigation.push('Quizz', {
+        questionNumber: this.state.questionNumber+1,
+      })
+  }
 
-  if(questionNumber==5)
-    navigation.navigate('Score');
-
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Question number {questionNumber}</Text>
-      <Text></Text>
-      <Button
-          title="Tak"
-          onPress={() => {
-            navigation.push('Quizz', {
-              questionNumber: questionNumber+1,
-              answer: 1
-            })}}
-        />
-      <Button
-        title="Nie"
-        onPress={() => {
-          navigation.push('Quizz', {
-            questionNumber: questionNumber+1,
-            answer: 0
-          })}}
-      />
-    </View>
-  );
+  render(){
+    const state = {...this.state};
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        { state.questionsLib[state.questionNumber-1] != undefined ?
+        <View>
+          <Text style={{textAlignVertical:'top'}}>Question number {state.questionsLib[state.questionNumber-1].id}</Text>
+          <Text style={{fontSize: 20, fontWeight:'bold'}}>{state.questionsLib[state.questionNumber-1].question}</Text>
+          <Button
+              id="yes"
+              title="Yes"
+              onPress={()=>this.QuizzValidator('yes')}
+            />
+          <Button
+            id="no"
+            title="No"
+            onPress={()=>this.QuizzValidator('no')}
+          />
+        </View>
+        :
+        <View>
+          <Text style={{fontSize: 20, fontWeight:'bold'}}>Quizz is over, check your score</Text>
+          <Button
+            title="Score"
+            onPress={()=>{this.state.navigation.push('Score')}}
+          />
+        </View>
+        }
+      </View>
+    );
+  }
 }
 
 function ScoreScreen({navigation}) {
+  const finalScore = score;
+  score = 0;
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Your Score is {score}</Text>
+      <Text>Your Score is {finalScore}</Text>
       <Button
         title="Try again..."
         onPress={() => navigation.popToTop()}
